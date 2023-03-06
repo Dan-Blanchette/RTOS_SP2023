8bfea11411c1c47525619b06efe917d662387b96
Dan-Blanchette <blan5568@vandals.uidaho.edu>
2023-03-05 21:29:05 -0800

    trying to fix formatting

--- pico/Vandalino3PicoTestSuite/V3-7SEGTEST/main.c
+++ pico/Vandalino3PicoTestSuite/V3-7SEGTEST/main.c
@@ -7,16 +7,14 @@
  * @date Started: 2023-02-3, Due: 2023-02-8
  * Total Hours: 21 (coding and research)
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 #include "main.h"
 #include "hdc1080Drivers.h"
 
-
 /*PICO PIN SETUP*/
 // D13 Pin Assignment pico
 const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-
 
 /*SEMAPHORES*/
 // Semaphore intilizaition
@@ -32,18 +30,16 @@ static QueueHandle_t xQueueDisp = NULL;
 // pico D13 Blink
 static QueueHandle_t xQueuePico = NULL;
 
-
 /***********************
  * 						  *
  *       TASKS         *
  * 						  *
  ***********************/
 
-
 // PRIORITY 5
 void task_buttons()
 {
-	while(1)
+	while (1)
 	{
 		int button1, button2, button3;
 		button1 = gpio_get(BUTTON1);
@@ -55,34 +51,31 @@ void task_buttons()
 	}
 }
 
-
 // Starts at draws the 0-9 count, 9-0 count on the right display
 // PRIORITY: 4
 void task_right_disp()
 {
-	
+
 	while (1)
 	{
 		// Take the semaphore
 		xSemaphoreTake(xSem, portMAX_DELAY);
-		//printf("In right display\n");
+		// printf("In right display\n");
 		gpio_put(SevenSegCC1, 1);
 		gpio_put(SevenSegCC2, 0);
 		int rec_val = 0;
 		xQueuePeek(xQueueDisp, &rec_val, portMAX_DELAY);
 		int right_num = (rec_val % 10);
-		
+
 		if (right_num < 10)
-		{	
+		{
 			// draw the numbers to the display
 			draw_numbers(right_num);
 		}
 
-		
 		vTaskDelay(10 / portTICK_PERIOD_MS);
 		xSemaphoreGive(xSem);
 		vTaskDelay(10 / portTICK_PERIOD_MS);
-
 	}
 }
 
@@ -93,7 +86,7 @@ void task_left_disp()
 	while (1)
 	{
 		xSemaphoreTake(xSem, portMAX_DELAY);
-		//printf("In left display\n");
+		// printf("In left display\n");
 		gpio_put(SevenSegCC1, 0);
 		gpio_put(SevenSegCC2, 1);
 
@@ -122,7 +115,7 @@ void task_count()
 	for (int i = 42; i >= 0; i--)
 	{
 
-		//printf("decrementing %d\n", i);
+		// printf("decrementing %d\n", i);
 
 		val_to_send = i;
 		bin_val_to_send;
@@ -145,7 +138,7 @@ void task_count()
 				xQueueSend(xQueueDisp, &val_to_send, 0U);
 				xQueueSend(xQueuePico, &bin_val_to_send, 0U);
 				vTaskDelay(500 / portTICK_PERIOD_MS);
-	
+
 				// clear the queue when done
 				xQueueReceive(xQueueDisp, &val_to_send, 0U);
 			}
@@ -154,10 +147,9 @@ void task_count()
 	vTaskDelay(10 / portTICK_PERIOD_MS);
 }
 
-
 /**
  * @brief HUMIDITY SENSOR TASK
- * 
+ *
  */
 void task_humiditySensor()
 {
@@ -184,7 +176,6 @@ void task_humiditySensor()
 	}
 }
 
-
 // blink pico
 void task_pico_blink()
 {
@@ -210,8 +201,6 @@ void task_pico_blink()
 	}
 }
 
-
-
 /***********************
  * 						  *
  *    MAIN DRIVER 	  *
@@ -225,51 +214,49 @@ int main()
 	// setup the buttons
 	setup_buttons();
 
-  // Init Pico
-  	gpio_init(LED_PIN);
+	// Init Pico
+	gpio_init(LED_PIN);
 	gpio_set_dir(LED_PIN, GPIO_OUT);
 
-  // Use for debugging
-  stdio_init_all(); // Initialize STD I/O for printing over serial
+	// Use for debugging
+	stdio_init_all(); // Initialize STD I/O for printing over serial
 
-  // setup HDC1080
-  setup_HDC1080();
- 
- /************** QUEUES AND SEMAPHORES ******************/
+	// setup HDC1080
+	setup_HDC1080();
 
- // creates Queue NOTE: like malloc specify data type
- xQueueDisp = xQueueCreate(1, sizeof(int));
- // create Pico Queue (has two values 0 or 1)
- xQueuePico = xQueueCreate(1, sizeof(int));
- // NOTE:create binary semaphore before tasks but after init!
- xSem = xSemaphoreCreateBinary();
- // take flag from semaphore
- xSemaphoreGive(xSem);
+	/************** QUEUES AND SEMAPHORES ******************/
 
-  
-/**************************** TASKS ************************************/
+	// creates Queue NOTE: like malloc specify data type
+	xQueueDisp = xQueueCreate(1, sizeof(int));
+	// create Pico Queue (has two values 0 or 1)
+	xQueuePico = xQueueCreate(1, sizeof(int));
+	// NOTE:create binary semaphore before tasks but after init!
+	xSem = xSemaphoreCreateBinary();
+	// take flag from semaphore
+	xSemaphoreGive(xSem);
 
-  xTaskCreate(task_buttons, "Task_Buttons", 256, NULL, 5, NULL);
-  xTaskCreate(
-              task_right_disp, // fucntion to be called
-             "Task_Right_Disp", // Name of Task 
-              256,  // Stack Size
-              NULL, // Parameter to pass to a function
-              4,  // Task Priority (0 to configMAX_PRIORITIES - 1)
-              NULL // Task handle (check on status, watch memory usage, or end the task)
-              );
-  xTaskCreate(task_left_disp, "Task_Left_Disp", 256, NULL, 4, NULL);
-  xTaskCreate(task_count, "Task_Count", 256, NULL, 3, NULL);
-  xTaskCreate(task_pico_blink, "Task_Pico_Blink", 256, NULL, 2, NULL);
-  xTaskCreate(task_humiditySensor, "Task_HumiditySensor", 256, NULL, 1, NULL);
-  // tell the scheduler to start running
-  vTaskStartScheduler();
+	/**************************** TASKS ************************************/
 
-  while (1){}
+	xTaskCreate(task_buttons, "Task_Buttons", 256, NULL, 5, NULL);
+	xTaskCreate(
+		 task_right_disp,	  // fucntion to be called
+		 "Task_Right_Disp", // Name of Task
+		 256,					  // Stack Size
+		 NULL,				  // Parameter to pass to a function
+		 4,					  // Task Priority (0 to configMAX_PRIORITIES - 1)
+		 NULL					  // Task handle (check on status, watch memory usage, or end the task)
+	);
+	xTaskCreate(task_left_disp, "Task_Left_Disp", 256, NULL, 4, NULL);
+	xTaskCreate(task_count, "Task_Count", 256, NULL, 3, NULL);
+	xTaskCreate(task_pico_blink, "Task_Pico_Blink", 256, NULL, 2, NULL);
+	xTaskCreate(task_humiditySensor, "Task_HumiditySensor", 256, NULL, 1, NULL);
+	// tell the scheduler to start running
+	vTaskStartScheduler();
+
+	while (1)
+	{
+	}
 }
-
-
-
 
 /***********************
  * 						  *
@@ -279,59 +266,56 @@ int main()
 
 /**
  * @brief Set the up 7seg display
- * 
+ *
  */
 void setup_7seg()
 {
-		// initialize digital pin LED_BUILTIN as an output.
-		gpio_init(SevenSegA);
-		gpio_init(SevenSegB);
-		gpio_init(SevenSegC);
-		gpio_init(SevenSegD);
-		gpio_init(SevenSegE);
-		gpio_init(SevenSegF);
-		gpio_init(SevenSegG);
-		// This GPIO pin activates the decimal point on the 7 segment display
-		gpio_init(SevenSegDP);
+	// initialize digital pin LED_BUILTIN as an output.
+	gpio_init(SevenSegA);
+	gpio_init(SevenSegB);
+	gpio_init(SevenSegC);
+	gpio_init(SevenSegD);
+	gpio_init(SevenSegE);
+	gpio_init(SevenSegF);
+	gpio_init(SevenSegG);
+	// This GPIO pin activates the decimal point on the 7 segment display
+	gpio_init(SevenSegDP);
 
-		gpio_init(SevenSegCC1);
-		gpio_init(SevenSegCC2);
+	gpio_init(SevenSegCC1);
+	gpio_init(SevenSegCC2);
 
-		gpio_set_dir(SevenSegA, GPIO_OUT);
-		gpio_set_dir(SevenSegB, GPIO_OUT);
-		gpio_set_dir(SevenSegC, GPIO_OUT);
-		gpio_set_dir(SevenSegD, GPIO_OUT);
-		gpio_set_dir(SevenSegE, GPIO_OUT);
-		gpio_set_dir(SevenSegF, GPIO_OUT);
-		gpio_set_dir(SevenSegG, GPIO_OUT);
-		gpio_set_dir(SevenSegDP, GPIO_OUT);
+	gpio_set_dir(SevenSegA, GPIO_OUT);
+	gpio_set_dir(SevenSegB, GPIO_OUT);
+	gpio_set_dir(SevenSegC, GPIO_OUT);
+	gpio_set_dir(SevenSegD, GPIO_OUT);
+	gpio_set_dir(SevenSegE, GPIO_OUT);
+	gpio_set_dir(SevenSegF, GPIO_OUT);
+	gpio_set_dir(SevenSegG, GPIO_OUT);
+	gpio_set_dir(SevenSegDP, GPIO_OUT);
 
-		gpio_set_dir(SevenSegCC1, GPIO_OUT);
-		gpio_set_dir(SevenSegCC2, GPIO_OUT);
+	gpio_set_dir(SevenSegCC1, GPIO_OUT);
+	gpio_set_dir(SevenSegCC2, GPIO_OUT);
 }
-
 
 /**
  * @brief Set the up V3 buttons
- * 
+ *
  */
 void setup_buttons()
 {
-	 // initialize digital pin LED_BUILTIN as an output.
-    gpio_init(BUTTON1);
-    gpio_init(BUTTON2);
-    gpio_init(BUTTON3);
+	// initialize digital pin LED_BUILTIN as an output.
+	gpio_init(BUTTON1);
+	gpio_init(BUTTON2);
+	gpio_init(BUTTON3);
 
-	 gpio_set_dir(BUTTON1, GPIO_IN);
-    gpio_set_dir(BUTTON2, GPIO_IN);
-    gpio_set_dir(BUTTON3, GPIO_IN);
-
+	gpio_set_dir(BUTTON1, GPIO_IN);
+	gpio_set_dir(BUTTON2, GPIO_IN);
+	gpio_set_dir(BUTTON3, GPIO_IN);
 }
-
 
 /**
  * @brief Set the up HDC1080 Sensor
- * 
+ *
  */
 void setup_HDC1080()
 {
@@ -346,178 +330,177 @@ void setup_HDC1080()
 	sleep_ms(1000);
 }
 
-
 /**
  * @brief send binary information to enable each segment
  * and draw the numbers
- * 
- * @param segNum 
+ *
+ * @param segNum
  */
 void draw_numbers(const int segNum)
 {
-  switch (segNum)
-  {
-   	case 0:
-    	/*
-         A
-         ___        __
-      F | G | B    |  |
-      E |___| C    |__|
-          D
-      */
-      	gpio_put(SevenSegA, 1);
-      	gpio_put(SevenSegB, 1);
-      	gpio_put(SevenSegC, 1);
-      	gpio_put(SevenSegD, 1);
-      	gpio_put(SevenSegE, 1);
-      	gpio_put(SevenSegF, 1);
-      	gpio_put(SevenSegG, 0);
-      	break;
+	switch (segNum)
+	{
+	case 0:
+		/*
+			A
+			___        __
+		F | G | B    |  |
+		E |___| C    |__|
+			 D
+		*/
+		gpio_put(SevenSegA, 1);
+		gpio_put(SevenSegB, 1);
+		gpio_put(SevenSegC, 1);
+		gpio_put(SevenSegD, 1);
+		gpio_put(SevenSegE, 1);
+		gpio_put(SevenSegF, 1);
+		gpio_put(SevenSegG, 0);
+		break;
 
-   	case 1:
+	case 1:
 
-   	// display #1
-   	/*
+		// display #1
+		/*
 			| B
-      	| C
-   	*/
-      	gpio_put(SevenSegA, 0);
-      	gpio_put(SevenSegB, 1);
-      	gpio_put(SevenSegC, 1);
-      	gpio_put(SevenSegD, 0);
-      	gpio_put(SevenSegE, 0);
-      	gpio_put(SevenSegF, 0);
-      	gpio_put(SevenSegG, 0);
-      	break;
-   
-		case 2:
-			// display #2 on the right segment
-			/* __
-				__|
-			  |__
-					*/
-			gpio_put(SevenSegA, 1);
-			gpio_put(SevenSegB, 1);
-			gpio_put(SevenSegC, 0);
-			gpio_put(SevenSegD, 1);
-			gpio_put(SevenSegE, 1);
-			gpio_put(SevenSegF, 0);
-			gpio_put(SevenSegG, 1);
-			break;
-		case 3:
-			// display #3 on the right segment
-			/* __
-				__|
-				__|
-					*/
-			gpio_put(SevenSegA, 1);
-			gpio_put(SevenSegB, 1);
-			gpio_put(SevenSegC, 1);
-			gpio_put(SevenSegD, 1);
-			gpio_put(SevenSegE, 0);
-			gpio_put(SevenSegF, 0);
-			gpio_put(SevenSegG, 1);
-			break;
-		case 4:
-			// display #4 on the right segment
-			/*
-				|__|
-					|
-					*/
-			gpio_put(SevenSegA, 0);
-			gpio_put(SevenSegB, 1);
-			gpio_put(SevenSegC, 1);
-			gpio_put(SevenSegD, 0);
-			gpio_put(SevenSegE, 0);
-			gpio_put(SevenSegF, 1);
-			gpio_put(SevenSegG, 1);
-			break;	
-		case 5:
-			// display #5 on the right segment
-			/*  __
-				|__
-				 __|*/
-			gpio_put(SevenSegA, 1);
-			gpio_put(SevenSegB, 0);
-			gpio_put(SevenSegC, 1);
-			gpio_put(SevenSegD, 1);
-			gpio_put(SevenSegE, 0);
-			gpio_put(SevenSegF, 1);
-			gpio_put(SevenSegG, 1);
-			break;
-		case 6:
-		   // display #6 on the right segment
-			/*  __
-				|__
-				|__|*/
-			gpio_put(SevenSegA, 1);
-			gpio_put(SevenSegB, 0);
-			gpio_put(SevenSegC, 1);
-			gpio_put(SevenSegD, 1);
-			gpio_put(SevenSegE, 1);
-			gpio_put(SevenSegF, 1);
-			gpio_put(SevenSegG, 1);
-			break;
-		case 7:
-			// display #7 on the right segment
-			/*
-				 __
-					|
-					|
-			*/
-			gpio_put(SevenSegA, 1);
-			gpio_put(SevenSegB, 1);
-			gpio_put(SevenSegC, 1);
-			gpio_put(SevenSegD, 0);
-			gpio_put(SevenSegE, 0);
-			gpio_put(SevenSegF, 0);
-			gpio_put(SevenSegG, 0);
-			break;
-		case 8:
-			// display #8 on the right segment
-			/*  __
-				|__|
-				|__|*/
-			gpio_put(SevenSegA, 1);
-			gpio_put(SevenSegB, 1);
-			gpio_put(SevenSegC, 1);
-			gpio_put(SevenSegD, 1);
-			gpio_put(SevenSegE, 1);
-			gpio_put(SevenSegF, 1);
-			gpio_put(SevenSegG, 1);
-			break;
-		case 9:
-			// display #9 on the right segment
-			/*  __
-				|__|
-					|
-					*/
-			gpio_put(SevenSegA, 1);
-			gpio_put(SevenSegB, 1);
-			gpio_put(SevenSegC, 1);
-			gpio_put(SevenSegD, 0);
-			gpio_put(SevenSegE, 0);
-			gpio_put(SevenSegF, 1);
-			gpio_put(SevenSegG, 1);
-			break;
-			
-    	default:
-			// this is for debug purposes
-			printf("Please enter a value between 0-9");
-  }
+			| C
+		*/
+		gpio_put(SevenSegA, 0);
+		gpio_put(SevenSegB, 1);
+		gpio_put(SevenSegC, 1);
+		gpio_put(SevenSegD, 0);
+		gpio_put(SevenSegE, 0);
+		gpio_put(SevenSegF, 0);
+		gpio_put(SevenSegG, 0);
+		break;
+
+	case 2:
+		// display #2 on the right segment
+		/* __
+			__|
+		  |__
+				*/
+		gpio_put(SevenSegA, 1);
+		gpio_put(SevenSegB, 1);
+		gpio_put(SevenSegC, 0);
+		gpio_put(SevenSegD, 1);
+		gpio_put(SevenSegE, 1);
+		gpio_put(SevenSegF, 0);
+		gpio_put(SevenSegG, 1);
+		break;
+	case 3:
+		// display #3 on the right segment
+		/* __
+			__|
+			__|
+				*/
+		gpio_put(SevenSegA, 1);
+		gpio_put(SevenSegB, 1);
+		gpio_put(SevenSegC, 1);
+		gpio_put(SevenSegD, 1);
+		gpio_put(SevenSegE, 0);
+		gpio_put(SevenSegF, 0);
+		gpio_put(SevenSegG, 1);
+		break;
+	case 4:
+		// display #4 on the right segment
+		/*
+			|__|
+				|
+				*/
+		gpio_put(SevenSegA, 0);
+		gpio_put(SevenSegB, 1);
+		gpio_put(SevenSegC, 1);
+		gpio_put(SevenSegD, 0);
+		gpio_put(SevenSegE, 0);
+		gpio_put(SevenSegF, 1);
+		gpio_put(SevenSegG, 1);
+		break;
+	case 5:
+		// display #5 on the right segment
+		/*  __
+			|__
+			 __|*/
+		gpio_put(SevenSegA, 1);
+		gpio_put(SevenSegB, 0);
+		gpio_put(SevenSegC, 1);
+		gpio_put(SevenSegD, 1);
+		gpio_put(SevenSegE, 0);
+		gpio_put(SevenSegF, 1);
+		gpio_put(SevenSegG, 1);
+		break;
+	case 6:
+		// display #6 on the right segment
+		/*  __
+			|__
+			|__|*/
+		gpio_put(SevenSegA, 1);
+		gpio_put(SevenSegB, 0);
+		gpio_put(SevenSegC, 1);
+		gpio_put(SevenSegD, 1);
+		gpio_put(SevenSegE, 1);
+		gpio_put(SevenSegF, 1);
+		gpio_put(SevenSegG, 1);
+		break;
+	case 7:
+		// display #7 on the right segment
+		/*
+			 __
+				|
+				|
+		*/
+		gpio_put(SevenSegA, 1);
+		gpio_put(SevenSegB, 1);
+		gpio_put(SevenSegC, 1);
+		gpio_put(SevenSegD, 0);
+		gpio_put(SevenSegE, 0);
+		gpio_put(SevenSegF, 0);
+		gpio_put(SevenSegG, 0);
+		break;
+	case 8:
+		// display #8 on the right segment
+		/*  __
+			|__|
+			|__|*/
+		gpio_put(SevenSegA, 1);
+		gpio_put(SevenSegB, 1);
+		gpio_put(SevenSegC, 1);
+		gpio_put(SevenSegD, 1);
+		gpio_put(SevenSegE, 1);
+		gpio_put(SevenSegF, 1);
+		gpio_put(SevenSegG, 1);
+		break;
+	case 9:
+		// display #9 on the right segment
+		/*  __
+			|__|
+				|
+				*/
+		gpio_put(SevenSegA, 1);
+		gpio_put(SevenSegB, 1);
+		gpio_put(SevenSegC, 1);
+		gpio_put(SevenSegD, 0);
+		gpio_put(SevenSegE, 0);
+		gpio_put(SevenSegF, 1);
+		gpio_put(SevenSegG, 1);
+		break;
+
+	default:
+		// this is for debug purposes
+		printf("Please enter a value between 0-9");
+	}
 }
 
 /**
  * @brief if the values being calculated are remainders
  * 10 - 15 substitute the hex values instead of decimal
- * @param rem 
+ * @param rem
  */
 void draw_hex_val(const int rem)
 {
 	switch (rem)
 	{
-		// draw  a
-		case 10:
+	// draw  a
+	case 10:
 		gpio_put(SevenSegA, 1);
 		gpio_put(SevenSegB, 1);
 		gpio_put(SevenSegC, 1);
@@ -527,8 +510,8 @@ void draw_hex_val(const int rem)
 		gpio_put(SevenSegG, 1);
 		break;
 
-		// draw b
-		case 11:
+	// draw b
+	case 11:
 		gpio_put(SevenSegA, 0);
 		gpio_put(SevenSegB, 0);
 		gpio_put(SevenSegC, 1);
@@ -538,8 +521,8 @@ void draw_hex_val(const int rem)
 		gpio_put(SevenSegG, 1);
 		break;
 
-		// draw c
-		case 12:
+	// draw c
+	case 12:
 		gpio_put(SevenSegA, 0);
 		gpio_put(SevenSegB, 0);
 		gpio_put(SevenSegC, 0);
@@ -549,8 +532,8 @@ void draw_hex_val(const int rem)
 		gpio_put(SevenSegG, 1);
 		break;
 
-		// draw d
-		case 13:
+	// draw d
+	case 13:
 		gpio_put(SevenSegA, 0);
 		gpio_put(SevenSegB, 1);
 		gpio_put(SevenSegC, 1);
@@ -560,8 +543,8 @@ void draw_hex_val(const int rem)
 		gpio_put(SevenSegG, 1);
 		break;
 
-		// draw e
-		case 14:
+	// draw e
+	case 14:
 		gpio_put(SevenSegA, 1);
 		gpio_put(SevenSegB, 0);
 		gpio_put(SevenSegC, 0);
@@ -571,8 +554,8 @@ void draw_hex_val(const int rem)
 		gpio_put(SevenSegG, 1);
 		break;
 
-		// draw f
-		case 15:
+	// draw f
+	case 15:
 		gpio_put(SevenSegA, 1);
 		gpio_put(SevenSegB, 0);
 		gpio_put(SevenSegC, 0);
@@ -582,8 +565,8 @@ void draw_hex_val(const int rem)
 		gpio_put(SevenSegG, 1);
 		break;
 
-		default:
-			draw_numbers(rem);
+	default:
+		draw_numbers(rem);
 		break;
 	}
 }
@@ -638,9 +621,7 @@ float temperature(enum Temp_Type degrees, enum Resolution_Type resolution)
 	return (final_temp * 1.8) + 32; // degrees F conversion
 }
 
-
 /*HDC1080 FUNCTION DEFINITIONS*/
-
 
 /**
  * @brief
@@ -656,18 +637,18 @@ int readDeviceID()
 	/*Assign Register*/
 	uint8_t manReg = HDC1080_MANF_DEVICE_ID_REG;
 
-	ret = i2c_write_blocking(I2C_PORT,  // type of port
-	   HDC1080ADDRESS, // device address
-	   &manReg,       // device's register address to read
-	   1,		// expected data size to receive in bytes
-	   false       // bool value to tell the I2C controller to: True = use and hold onto the bus, False = release the bus
+	ret = i2c_write_blocking(I2C_PORT,		  // type of port
+									 HDC1080ADDRESS, // device address
+									 &manReg,		  // device's register address to read
+									 1,				  // expected data size to receive in bytes
+									 false			  // bool value to tell the I2C controller to: True = use and hold onto the bus, False = release the bus
 	);
 
 	ret = i2c_read_blocking(I2C_PORT,		 // type of port
-		   HDC1080ADDRESS, // device address
-		   deviceID,	   // pass the uint_8 array to receive the data from the register
-		   2,		  // expected data size to receive in bytes
-		   false	 // bool value to tell the I2C controller to: True = use the bus, False = release the bus
+									HDC1080ADDRESS, // device address
+									deviceID,		 // pass the uint_8 array to receive the data from the register
+									2,					 // expected data size to receive in bytes
+									false				 // bool value to tell the I2C controller to: True = use the bus, False = release the bus
 	);
 
 	int returnValue = deviceID[0] << 8 | deviceID[1];
@@ -805,7 +786,7 @@ float calc_humidity(enum Resolution_Type resolution)
 	// write humidity reading to register
 	int hum_val = i2c_write_blocking(I2C_PORT, HDC1080ADDRESS, &HUMIDITY_REG, 1, false);
 
-	if(resolution == HIGH)
+	if (resolution == HIGH)
 	{
 		sleep_ms(9);
 	}
@@ -821,7 +802,7 @@ float calc_humidity(enum Resolution_Type resolution)
 	hum_val = i2c_read_blocking(I2C_PORT, HDC1080ADDRESS, data, 2, false);
 
 	int16_t bit_val = data[0] << 8 | data[1];
-	double hex_conv = ((double) bit_val) / ((double)65536);
+	double hex_conv = ((double)bit_val) / ((double)65536);
 	float tot_hum = hex_conv * 100;
 	return tot_hum;
 }
